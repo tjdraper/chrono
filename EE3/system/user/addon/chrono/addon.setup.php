4012aee60cb353e924338dc55ded5363d0763b83
Ryan Payne <rpayne@lewiscommunications.com>
2017-10-09 17:28:12 -0500

    Add EE3 compatible version

---
+++ EE3/system/user/addon/chrono/addon.setup.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	'author' => 'TJ Draper',
+	'author_url' => 'https://buzzingpixel.com',
+	'description' => 'Get years and months of channel entries',
+	'docs_url' => 'https://github.com/tjdraper/chrono',
+	'name' => 'Chrono',
+	'namespace' => 'BuzzingPixel\Chrono',
+	'version' => '1.1.0'
+];
