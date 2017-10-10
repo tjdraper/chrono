3d8ca64d4a8561134c90f226dc249d1c6fb97625
TJ Draper <tj@buzzingpixel.com>
2017-10-10 09:38:44 -0500

    Update version number in addon.setup.php

--- chrono/addon.setup.php
+++ chrono/addon.setup.php
@@ -13,5 +13,5 @@ return array(
     'docs_url' => 'https://github.com/tjdraper/chrono',
     'name' => 'Chrono',
     'namespace' => 'BuzzingPixel\Chrono',
-    'version' => '1.1.0'
+    'version' => '2.0.0'
 );
